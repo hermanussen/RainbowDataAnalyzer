ac886216953bf4964754c8f2007c3f03219beaf7
Robin Hermanussen <r.hermanussen@lukkien.com>
2017-02-11 20:54:29 +0100

    Added a basic form of completion (dummy implementation). Based on example here: https://msdn.microsoft.com/en-us/library/ee372314.aspx

---
+++ RainbowDataAnalyzer/RainbowDataAnalyzer.Vsix/TestCompletionHandlerProvider.cs
@@ -0,0 +1,40 @@
+﻿namespace VSIXProject1
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Editor;
+    using Microsoft.VisualStudio.Language.Intellisense;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.TextManager.Interop;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(IVsTextViewCreationListener))]
+    [Name("token completion handler")]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal class TestCompletionHandlerProvider : IVsTextViewCreationListener
+    {
+        [Import]
+        internal IVsEditorAdaptersFactoryService AdapterService = null;
+        [Import]
+        internal ICompletionBroker CompletionBroker { get; set; }
+
+        [Import]
+        internal SVsServiceProvider ServiceProvider { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            Func<TestCompletionCommandHandler> createCommandHandler = delegate()
+            {
+                return new TestCompletionCommandHandler(textViewAdapter, textView, this);
+            };
+
+            textView.Properties.GetOrCreateSingletonProperty(createCommandHandler);
+        }
+    }
+}
---
+++ RainbowDataAnalyzer/RainbowDataAnalyzer.Vsix/TestCompletionSourceProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace VSIXProject1
+{
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Language.Intellisense;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Operations;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("text")]
+    [Name("token completion")]
+    internal class TestCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new TestCompletionSource(this, textBuffer);
+        }
+    }
+}
